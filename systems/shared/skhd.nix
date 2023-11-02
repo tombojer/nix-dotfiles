14c870c5fc141be03e7ce347901816f1155993fa
Kevin Rudde <k.rudde@shopware.com>
2023-11-02 19:48:54 +0100

    Added mission control hotkeys

--- systems/shared/skhd.nix
+++ systems/shared/skhd.nix
@@ -27,23 +27,19 @@
       lalt - h : yabai -m window --toggle bsp
       lalt - v : yabai -m window --toggle split
 
-      lalt - 1 : yabai -m space --focus 1
-      lalt - 2 : yabai -m space --focus 2
-      lalt - 3 : yabai -m space --focus 3
-      lalt - 4 : yabai -m space --focus 4
-      lalt - 5 : yabai -m space --focus 5
-      lalt - 6 : yabai -m space --focus 6
-      lalt - 7 : yabai -m space --focus 7
-      lalt - 8 : yabai -m space --focus 8
-      lalt - 9 : yabai -m space --focus 9
-      lalt - 0 : yabai -m space --focus 10
+      lalt - 1 : yabai -m space --focus 1 && skhd -k "alt + cmd + ctrl + shift - 1"
+      lalt - 2 : yabai -m space --focus 2 && skhd -k "alt + cmd + ctrl + shift - 2"
+      lalt - 3 : yabai -m space --focus 3 && skhd -k "alt + cmd + ctrl + shift - 3"
+      lalt - 4 : yabai -m space --focus 4 && skhd -k "alt + cmd + ctrl + shift - 4"
+      lalt - 5 : yabai -m space --focus 5 && skhd -k "alt + cmd + ctrl + shift - 5"
+      lalt - 6 : yabai -m space --focus 6 && skhd -k "alt + cmd + ctrl + shift - 6"
 
-      lalt + shift - 1 : yabai -m window --space 1 && yabai -m space --focus 1
-      lalt + shift - 2 : yabai -m window --space 2 && yabai -m space --focus 2
-      lalt + shift - 3 : yabai -m window --space 3 && yabai -m space --focus 3
-      lalt + shift - 4 : yabai -m window --space 4 && yabai -m space --focus 4
-      lalt + shift - 5 : yabai -m window --space 5 && yabai -m space --focus 5
-      lalt + shift - 6 : yabai -m window --space 6 && yabai -m space --focus 6
+      lalt + shift - 1 : yabai -m window --space 1 && yabai -m space --focus 1 && skhd -k "alt + cmd + ctrl + shift - 1"
+      lalt + shift - 2 : yabai -m window --space 2 && yabai -m space --focus 2 && skhd -k "alt + cmd + ctrl + shift - 2"
+      lalt + shift - 3 : yabai -m window --space 3 && yabai -m space --focus 3 && skhd -k "alt + cmd + ctrl + shift - 3"
+      lalt + shift - 4 : yabai -m window --space 4 && yabai -m space --focus 4 && skhd -k "alt + cmd + ctrl + shift - 4"
+      lalt + shift - 5 : yabai -m window --space 5 && yabai -m space --focus 5 && skhd -k "alt + cmd + ctrl + shift - 5"
+      lalt + shift - 6 : yabai -m window --space 6 && yabai -m space --focus 6 && skhd -k "alt + cmd + ctrl + shift - 6"
 
       lalt - f : yabai -m window --toggle float
       lalt + shift - q : yabai -m window --close
