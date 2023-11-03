48eb232a4bd212a60f045a9fe735d7331f866596
Kevin Rudde <k.rudde@shopware.com>
2023-11-03 10:53:41 +0100

    Added volta to fish config

--- home/features/shell/default.nix
+++ home/features/shell/default.nix
@@ -30,12 +30,18 @@
       end
       set -el oldPath
 
+
+      # Homebrew config
       set -gx HOMEBREW_PREFIX "/opt/homebrew";
       set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
       set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
       ! set -q PATH; and set PATH \'\'; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
       ! set -q MANPATH; and set MANPATH \'\'; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
       ! set -q INFOPATH; and set INFOPATH \'\'; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
+
+      # Volta
+      set -gx VOLTA_HOME $HOME/.volta
+      fish_add_path $VOLTA_HOME/bin
     '';
 
     plugins = [
