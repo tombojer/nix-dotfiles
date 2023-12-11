dd4f243252dae8d246d1ee86cd28610369a67dbb
Kevin Rudde <k.rudde@shopware.com>
2023-12-11 14:19:31 +0100

    Add dedicated colima config

--- home/features/packages/default.nix
+++ home/features/packages/default.nix
@@ -1,5 +1,9 @@
 { pkgs, flake, ... }: {
 
+  imports = [
+    ./colima.nix
+  ];
+
   home.packages = with pkgs; [
     flake.inputs.devenv.packages.${system}.devenv
     cachix
@@ -18,7 +22,6 @@
     wireguard-tools
     mysql80
     kubectl
-    colima
     docker-client
     dive
     bun
