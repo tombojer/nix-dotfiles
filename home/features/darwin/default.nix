c17b3f38a3fefc3c3e5f178d99a6843bd19a259c
Kevin Rudde <k.rudde@shopware.com>
2024-09-11 10:46:41 +0200

    Update darwin keybindings

--- home/features/darwin/default.nix
+++ home/features/darwin/default.nix
@@ -2,11 +2,26 @@
 , pkgs
 , lib
 , flake
-, ... }:
+, ...
+}:
 
 {
   targets.darwin.keybindings = {
-    "^u" = "deleteToBeginningOfLine:";
-    "^w" = "deleteWordBackward:";
+    "\UF729" = "moveToBeginningOfParagraph:"; # home
+    "\UF72B" = "moveToEndOfParagraph:"; # end
+    "$\UF729" = "moveToBeginningOfParagraphAndModifySelection:"; # shift-home
+    "$\UF72B" = "moveToEndOfParagraphAndModifySelection:"; # shift-end
+    "^\UF729" = "moveToBeginningOfDocument:"; # ctrl-home
+    "^\UF72B" = "moveToEndOfDocument:"; # ctrl-end
+    "^$\UF729" = "moveToBeginningOfDocumentAndModifySelection:"; # ctrl-shift-home
+    "^$\UF72B" = "moveToEndOfDocumentAndModifySelection:"; # ctrl-shift-end
+    "^\UF702" = "moveWordLeft:"; # ctrl-left
+    "^$\UF702" = "moveWordLeftAndModifySelection:"; # ctrl-shift-left
+    "^\UF703" = "moveWordRight:"; # ctrl-right
+    "^$\UF703" = "moveWordRightAndModifySelection:"; # ctrl-shift-right
+    "@\UF702" = "moveWordLeft:"; # cmd-left
+    "@$\UF702" = "moveWordLeftAndModifySelection:"; # cmd-shift-left
+    "@\UF703" = "moveWordRight:"; # cmd-right
+    "@$\UF703" = "moveWordRightAndModifySelection:"; # cmd-shift-right
   };
 }
